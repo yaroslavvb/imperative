183dc9687d02f3f06c2c9387a6329a9010ba61a6
Yaroslav Bulatov <yaroslavvb@gmail.com>
2016-07-26 12:59:14 -0700

    update test paths

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
-# script must be run from tests directory
-export PYTHONPATH=..:$PYTHONPATH
 BASEDIR=$(dirname "$0")
+echo "Running simple test"
+python $BASEDIR/simple.py
 echo "Running env_test.py"
 python $BASEDIR/env_test.py
 echo "Running env_cache_test.py"
 python $BASEDIR/env_cache_test.py
-echo "Running extra_tests/batchnorm_test.py"
-python $BASEDIR/extra_tests/batchnorm_test.py
-echo "Running extra_tests/histogram_ops_test.py"
-python $BASEDIR/extra_tests/histogram_ops_test.py
-echo "Running extra_tests/math_ops_test.py"
-python $BASEDIR/extra_tests/math_ops_test.py
-echo "Running extra_tests/nn_test.py"
-python $BASEDIR/extra_tests/nn_test.py
+echo "Running extra/batchnorm_test.py"
+python $BASEDIR/extra/batchnorm_test.py
+echo "Running extra/histogram_ops_test.py"
+python $BASEDIR/extra/histogram_ops_test.py
+echo "Running extra/math_ops_test.py"
+python $BASEDIR/extra/math_ops_test.py
+echo "Running extra/nn_test.py"
+python $BASEDIR/extra/nn_test.py
 echo "Running itensor_test.py"
 python $BASEDIR/itensor_test.py
 echo "Running module_rewriter_test.py"
@@ -23,6 +23,6 @@ python $BASEDIR/lbfgs_test.py
 echo "Running mnist_inference_test.py"
 python $BASEDIR/mnist_inference_test.py
 # echo "Running image_ops_double_test"
-# python extra_tests/image_ops_double_test.py
+# python extra/image_ops_double_test.py
 # echo "Running image_ops_test"
-# python extra_tests/image_ops_test.py
+# python extra/image_ops_test.py
